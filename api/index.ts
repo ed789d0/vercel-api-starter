d0af4f1f01874d76c185a09e533455ca0fa06b43
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2020-08-28 12:13:36 -0400

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,27 +1,12 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-import { https }  from "https";
+//import { https }  from "https";
 export default (req: NowRequest, res: NowResponse) => {
   let c = 0;
-  https.get(decodeURI(req.body.jsonfile), (resp) => {
-        console.log(resp);
-        let data = '';
-        let q;
-        // A chunk of data has been recieved.
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
+  import q from "json!" + decodeURIComponent(req.body.jsonfile);
+  console.log(q);
 
-        // The whole response has been received. Print out the result.
-        resp.on('end', () => {
-            q = JSON.parse(data);
-        });
-        
-        for (var key in q) {
-            c += q[key].length;
-        }
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-    });
+  for (var key in q) {
+      c += q[key].length;
   }
   return res.json({ message: c });
 };
