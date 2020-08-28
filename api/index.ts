a3cf826989a96246d1b37f916cfe46d3bd64c876
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2020-08-28 12:06:01 -0400

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-
+import { https }  from "https";
 export default (req: NowRequest, res: NowResponse) => {
   let c = 0;
   https.get(decodeURI(req.body.jsonfile), (resp) => {
